80ff157e480b133784cd8c6474e124cbb504b2f7
CezaryMichalczyk <camichalczyk@gmail.com>
2023-12-05 11:54:08 +0100

    Day 5 part 1

--- AdventOfCode2023/Day5/Day5Logic.cs
+++ AdventOfCode2023/Day5/Day5Logic.cs
@@ -5,21 +5,51 @@ namespace AdventOfCode2023.Day5
 {
     public class Day5Logic : IPuzzles
     {
-        private const string fileName = "day5_Input_test.in";
+        private const string fileName = "day5_Input.in";
         private const Int32 BufferSize = 128;
 
         public string FirstPuzzle()
         {
-            int result = 0;
+            long result = 0;
 
             using (var fileStream = File.OpenRead(fileName))
             using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
             {
                 string? line;
+                line = streamReader.ReadLine();
+                var seeds = line.Split("seeds:")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToList();
+                var guards = Enumerable.Repeat(false, seeds.Count).ToList();
+
                 while ((line = streamReader.ReadLine()) != null)
                 {
+                    line = line.Trim();
                     
+                    if (line.Contains("map"))
+                    {
+                        guards = Enumerable.Repeat(false, seeds.Count).ToList();
+                        continue;
+                    }
+                    else if (line == "")
+                    {
+                        continue;
+                    }
+
+                    var singleLine = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToList();
+                    var destinationRangeStart = singleLine[0];
+                    var sourceRangeStart = singleLine[1];
+                    var rangeLength = singleLine[2];
+
+                    for(int i = 0; i < seeds.Count; i++)
+                    {
+                        if (guards[i] == false && seeds[i] >= sourceRangeStart && seeds[i] <= sourceRangeStart + rangeLength)
+                        {
+                            seeds[i] = seeds[i] + destinationRangeStart - sourceRangeStart;
+                            guards[i] = true;
+                        }
+                    }
                 }
+
+                result = seeds.Min(x => x);
             }
 
             return result.ToString();
