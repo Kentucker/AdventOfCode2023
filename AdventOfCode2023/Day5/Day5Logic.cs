1468a6ac8aca72588101aedbc031587c9e59d353
CezaryMichalczyk <camichalczyk@gmail.com>
2023-12-05 23:10:15 +0100

    Day 5 Part 2

--- AdventOfCode2023/Day5/Day5Logic.cs
+++ AdventOfCode2023/Day5/Day5Logic.cs
@@ -23,7 +23,7 @@ namespace AdventOfCode2023.Day5
                 while ((line = streamReader.ReadLine()) != null)
                 {
                     line = line.Trim();
-                    
+
                     if (line.Contains("map"))
                     {
                         guards = Enumerable.Repeat(false, seeds.Count).ToList();
@@ -39,9 +39,9 @@ namespace AdventOfCode2023.Day5
                     var sourceRangeStart = singleLine[1];
                     var rangeLength = singleLine[2];
 
-                    for(int i = 0; i < seeds.Count; i++)
+                    for (int i = 0; i < seeds.Count; i++)
                     {
-                        if (guards[i] == false && seeds[i] >= sourceRangeStart && seeds[i] <= sourceRangeStart + rangeLength)
+                        if (guards[i] == false && seeds[i] >= sourceRangeStart && seeds[i] < sourceRangeStart + rangeLength)
                         {
                             seeds[i] = seeds[i] + destinationRangeStart - sourceRangeStart;
                             guards[i] = true;
@@ -57,19 +57,140 @@ namespace AdventOfCode2023.Day5
 
         public string SecondPuzzle()
         {
-            int result = 0;
+            var almanac = ReadAlmanac(fileName);
 
-            using (var fileStream = File.OpenRead(fileName))
-            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            var minimum = almanac.Seeds.Max(x => x);
+
+            var tasksList = new List<Task<long>>();
+
+            for (int j = 0; j < almanac.Seeds.Count; j += 2)
             {
-                string? line;
-                while ((line = streamReader.ReadLine()) != null)
-                {
+                var currentPair = j;
+                var task = Task.Run(() => ProcessSeedsForRange(almanac, currentPair));
+                tasksList.Add(task);
+            }
 
+            Task.WaitAll([.. tasksList]);
+
+            minimum = tasksList.Min(x => x.Result);
+
+            return minimum.ToString();
+        }
+
+        private long ProcessSingleSeed(List<Mapper> mappings, long currentSeedNumber)
+        {
+            foreach (var mapping in mappings)
+            {
+                foreach (var map in mapping.Maps)
+                {
+                    if (currentSeedNumber >= map.SourceRangeStart && currentSeedNumber < map.SourceRangeStart + map.RangeLength)
+                    {
+                        currentSeedNumber = currentSeedNumber + map.DestinationRangeStart - map.SourceRangeStart;
+                        break;
+                    }
                 }
             }
 
-            return result.ToString();
+            return currentSeedNumber;
+        }
+
+        private long ProcessSeedsForRange(Almanac almanac, int index)
+        {
+            var localMinimum = almanac.Seeds.Max(x => x);
+
+            var from = almanac.Seeds[index];
+            var to = almanac.Seeds[index] + almanac.Seeds[index + 1];
+
+            for (long i = from; i < to; i++)
+            {
+                var currentSeedNumber = i;
+                var seedPosition = ProcessSingleSeed(almanac.Mappings, currentSeedNumber);
+
+                localMinimum = long.Min(seedPosition, localMinimum);
+            }
+
+            return localMinimum;
+        }
+
+        private Almanac ReadAlmanac(string fileName)
+        {
+            var file = File.ReadAllLines(fileName);
+            var fileLinesList = new List<string>(file);
+
+            var seeds = fileLinesList[0].Split("seeds:")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToList();
+
+            int lineNumber = 1;
+            int mapperNumber = -1;
+            List<Mapper> mappings = [];
+
+            while (lineNumber < fileLinesList.Count)
+            {
+                if (fileLinesList[lineNumber].Contains("map"))
+                {
+                    lineNumber++;
+                    continue;
+                }
+                else if (fileLinesList[lineNumber] == "")
+                {
+                    lineNumber++;
+                    mapperNumber++;
+                    mappings.Add(new Mapper(mapperNumber, []));
+                    continue;
+                }
+
+                var singleLine = fileLinesList[lineNumber].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToList();
+                var destinationRangeStart = singleLine[0];
+                var sourceRangeStart = singleLine[1];
+                var rangeLength = singleLine[2];
+
+                var map = new Map(destinationRangeStart, sourceRangeStart, rangeLength);
+                mappings.Where(x => x.Id == mapperNumber).FirstOrDefault().Maps.Add(map);
+
+                lineNumber++;
+            }
+
+
+            var almanac = new Almanac(seeds, mappings);
+
+            return almanac;
+        }
+
+        struct Almanac
+        {
+            public Almanac(List<long> seeds, List<Mapper> mappings)
+            {
+                Seeds = seeds;
+                Mappings = mappings;
+            }
+
+            public List<long> Seeds {  get; set; }
+            public List<Mapper> Mappings { get; set; }
+        }
+
+        struct Mapper
+        {
+            public Mapper(int id, List<Map> maps)
+            {
+                Id = id;
+                Maps = maps;
+            }
+
+            public int Id { get; set; }
+            public List<Map> Maps { get; set; }
+        }
+
+        struct Map
+        {
+            public Map(long destinationRangeStart, long sourceRangeStart, long rangeLength)
+            {
+                DestinationRangeStart = destinationRangeStart;
+                SourceRangeStart = sourceRangeStart;
+                RangeLength = rangeLength;
+            }
+
+            public long DestinationRangeStart { get; set; }
+            public long SourceRangeStart { get; set; }
+            public long RangeLength { get; set; }
         }
     }
 }
