5fe247e321296e12133a56b9d520285a3bd85b73
CezaryMichalczyk <camichalczyk@gmail.com>
2023-12-05 23:18:54 +0100

    Code refactor

--- AdventOfCode2023/Day5/Day5Logic.cs
+++ AdventOfCode2023/Day5/Day5Logic.cs
@@ -1,5 +1,4 @@
 ﻿using AdventOfCode2023.Interfaces;
-using System.Text;
 
 namespace AdventOfCode2023.Day5
 {
@@ -10,49 +9,19 @@ namespace AdventOfCode2023.Day5
 
         public string FirstPuzzle()
         {
-            long result = 0;
+            var almanac = ReadAlmanac(fileName);
 
-            using (var fileStream = File.OpenRead(fileName))
-            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            var localMinimum = almanac.Seeds.Max(x => x);
+
+            foreach (var seed in almanac.Seeds)
             {
-                string? line;
-                line = streamReader.ReadLine();
-                var seeds = line.Split("seeds:")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToList();
-                var guards = Enumerable.Repeat(false, seeds.Count).ToList();
+                var currentSeedNumber = seed;
+                var seedPosition = ProcessSingleSeed(almanac.Mappings, currentSeedNumber);
 
-                while ((line = streamReader.ReadLine()) != null)
-                {
-                    line = line.Trim();
-
-                    if (line.Contains("map"))
-                    {
-                        guards = Enumerable.Repeat(false, seeds.Count).ToList();
-                        continue;
-                    }
-                    else if (line == "")
-                    {
-                        continue;
-                    }
-
-                    var singleLine = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToList();
-                    var destinationRangeStart = singleLine[0];
-                    var sourceRangeStart = singleLine[1];
-                    var rangeLength = singleLine[2];
-
-                    for (int i = 0; i < seeds.Count; i++)
-                    {
-                        if (guards[i] == false && seeds[i] >= sourceRangeStart && seeds[i] < sourceRangeStart + rangeLength)
-                        {
-                            seeds[i] = seeds[i] + destinationRangeStart - sourceRangeStart;
-                            guards[i] = true;
-                        }
-                    }
-                }
-
-                result = seeds.Min(x => x);
+                localMinimum = long.Min(seedPosition, localMinimum);
             }
 
-            return result.ToString();
+            return localMinimum.ToString();
         }
 
         public string SecondPuzzle()
